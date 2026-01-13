e30b61110afd1b78e0128c0927ac60551f10f93a
kyruodioso <kanu_nirvana@hotmail.com>
2026-01-13 03:15:03 -0300

    Fix: Remove hardcoded default values for Sanity config

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your-project-id'
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
 
 export const client = createClient({
