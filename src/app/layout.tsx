7fb01a42a1003f9025948e15bc10db2273e77a88
kyruodioso <kanu_nirvana@hotmail.com>
2026-01-15 14:02:28 -0300

    chore: disable viewport zooming on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter, Oswald } from "next/font/google";
 import "./globals.css";
 
@@ -56,6 +56,13 @@ export const metadata: Metadata = {
     },
   },
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 import { Toaster } from 'sonner';
 
